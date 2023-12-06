a00a97530cdde4a2ed6b671ef8504de3c78eabff
ndreiic <149219254+ndreiic@users.noreply.github.com>
2023-12-07 00:27:33 +0800

    Add files via upload

---
+++ events.js
@@ -0,0 +1,276 @@
+// * COLLEGE OF EDUCATION - COLLEGE OF EDUCATION - COLLEGE OF EDUCATION - COLLEGE OF EDUCATION
+
+function toggleArrowBEEDCHED(){
+    var arrowToggle = document.querySelector('#toggleBEEDCHED');
+    var BEEDCourse = document.getElementById('courseDetailsBEEDCHED');
+    var displaySetting = BEEDCourse.style.display;
+    arrowToggle.classList.toggle('active');
+    document.getElementById('activeBEEDCHED').classList.toggle('bachelorOpen');
+
+    if(displaySetting == 'block'){
+        BEEDCourse.style.display = 'none'
+    }
+    else{
+        BEEDCourse.style.display = 'block'
+    }
+}
+
+function toggleArrowBSEDMIE(){
+    var arrowToggle = document.querySelector('#toggleBSEDMIE');
+    var BSEDMIECourse= document.getElementById('courseDetailsBSEDMIE');
+    var displaySetting = BSEDMIECourse.style.display;
+    arrowToggle.classList.toggle('active');
+    document.getElementById('activeBSEDMIE').classList.toggle('bachelorOpen');
+
+    if(displaySetting == 'block'){
+        BSEDMIECourse.style.display = 'none'
+    }
+    else{
+        BSEDMIECourse.style.display = 'block'
+    }
+}
+
+function toggleArrowBSEDMIF(){
+    var arrowToggle = document.querySelector('#toggleBSEDMIF');
+    var BSEDMIFCourse = document.getElementById('courseDetailsBSEDMIF');
+    var displaySetting = BSEDMIFCourse.style.display;
+    arrowToggle.classList.toggle('active');
+    document.getElementById('activeBSEDMIF').classList.toggle('bachelorOpen');
+
+    if(displaySetting == 'block'){
+        BSEDMIFCourse.style.display = 'none'
+    }
+    else{
+        BSEDMIFCourse.style.display = 'block'
+    }
+}
+
+function toggleArrowBSEDMIM(){
+    var arrowToggle = document.querySelector('#toggleBSEDMIM');
+    var BSEDMIMCourse = document.getElementById('courseDetailsBSEDMIM');
+    var displaySetting = BSEDMIMCourse.style.display;
+    arrowToggle.classList.toggle('active');
+    document.getElementById('activeBSEDMIM').classList.toggle('bachelorOpen');
+
+    if(displaySetting == 'block'){
+        BSEDMIMCourse.style.display = 'none'
+    }
+    else{
+        BSEDMIMCourse.style.display = 'block'
+    }
+}
+
+function toggleArrowBSEDMIS(){
+    var arrowToggle = document.querySelector('#toggleBSEDMIS');
+    var BSEDMISCourse = document.getElementById('courseDetailsBSEDMIS');
+    var displaySetting = BSEDMISCourse.style.display;
+    arrowToggle.classList.toggle('active');
+    document.getElementById('activeBSEDMIS').classList.toggle('bachelorOpen')
+
+    if(displaySetting == 'block'){
+        BSEDMISCourse.style.display = 'none'
+    }
+    else{
+        BSEDMISCourse.style.display = 'block'
+    }
+}
+
+function toggleArrowBSEDMISS(){
+    var arrowToggle = document.querySelector('#toggleBSEDMISS');
+    var BSEDMISSCourse = document.getElementById('courseDetailsCurvedBottomBSEDMISS');
+    var displaySetting = BSEDMISSCourse.style.display;
+    arrowToggle.classList.toggle('active');
+    document.getElementById('activeBSEDMISS').classList.toggle('bachelorOpenedCurvedBottom');
+
+    if(displaySetting == 'block'){
+        BSEDMISSCourse.style.display = 'none'
+    }
+    else{
+        BSEDMISSCourse.style.display = 'block'
+    }
+}
+
+
+
+// * COLLEGE OF ENGINEERING AND INFORMATION TECHNOLOGY - COLLEGE OF ENGINEERING AND INFORMATION TECHNOLOGY
+
+function toggleArrowBSCE(){
+    var arrowToggle = document.querySelector('#toggleBSCE');
+    var BSCECourse = document.getElementById('courseDetailsBSCE');
+    var displaySetting = BSCECourse.style.display;
+    arrowToggle.classList.toggle('active');
+    document.getElementById('activeBSCE').classList.toggle('bachelorOpen');
+
+    if(displaySetting == 'block'){
+        BSCECourse.style.display = 'none'
+    }
+    else{
+        BSCECourse.style.display = 'block'
+    }
+}
+
+function toggleArrowBSEE(){
+    var arrowToggle = document.querySelector('#toggleBSEE');
+    var BSEECourse = document.getElementById('courseDetailsBSEE');
+    var displaySetting = BSEECourse.style.display;
+    arrowToggle.classList.toggle('active');
+    document.getElementById('activeBSEE').classList.toggle('bachelorOpen');
+
+    if(displaySetting == 'block'){
+        BSEECourse.style.display = 'none'
+    }
+    else{
+        BSEECourse.style.display = 'block'
+    }
+}
+
+function toggleArrowBSIT(){
+    var arrowToggle = document.querySelector('#toggleBSIT');
+    var BSITCourse = document.getElementById('courseDetailsCurvedBottomBSIT');
+    var displaySetting = BSITCourse.style.display;
+    arrowToggle.classList.toggle('active');
+    document.getElementById('activeBSIT').classList.toggle('bachelorOpenedCurvedBottom')
+
+    if(displaySetting == 'block'){
+        BSITCourse.style.display = 'none'
+    }
+    else{
+        BSITCourse.style.display = 'block'
+    }
+}
+
+
+
+// * COLLEGE OF ARTS AND SCIENCES - COLLEGE OF ARTS AND SCIENCES - COLLEGE OF ARTS AND SCIENCES
+
+function toggleArrowBSIP(){
+    var arrowToggle = document.querySelector('#toggleBSIP');
+    var BSIPCourse = document.getElementById('courseDetailsBSIP');
+    var displaySetting = BSIPCourse.style.display;
+    arrowToggle.classList.toggle('active');
+    document.getElementById('activeBSIP').classList.toggle('bachelorOpen');
+
+    if(displaySetting == 'block'){
+        BSIPCourse.style.display = 'none'
+    }
+    else{
+        BSIPCourse.style.display = 'block'
+    }
+}
+
+function toggleArrowBSSW(){
+    var arrowToggle = document.querySelector('#toggleBSSW');
+    var BSSWCourse = document.getElementById('courseDetailsBSSW');
+    var displaySetting = BSSWCourse.style.display;
+    arrowToggle.classList.toggle('active');
+    document.getElementById('activeBSSW').classList.toggle('bachelorOpen');
+
+    if(displaySetting == 'block'){
+        BSSWCourse.style.display = 'none'
+    }
+    else{
+        BSSWCourse.style.display = 'block'
+    }
+}
+
+function toggleArrowBSC(){
+    var arrowToggle = document.querySelector('#toggleBSC');
+    var BSCCourse = document.getElementById('courseDetailsCurvedBottomBSC');
+    var displaySetting = BSCCourse.style.display;
+    arrowToggle.classList.toggle('active');
+    document.getElementById('activeBSC').classList.toggle('bachelorOpenedCurvedBottom');
+
+    if(displaySetting == 'block'){
+        BSCCourse.style.display = 'none'
+    }
+    else{
+        BSCCourse.style.display = 'block'
+    }
+}
+
+// * COLLEGE OF BUSINESS, ACCOUNTANCY, AND PUBLIC ADMINISTRATION - COLLEGE OF BUSINESS, ACCOUNTANCY, AND PUBLIC ADMINISTRATION
+
+function toggleArrowBSA(){
+    var arrowToggle = document.querySelector('#toggleBSA');
+    var BSACourse = document.getElementById('courseDetailsBSA');
+    var displaySetting = BSACourse.style.display;
+    arrowToggle.classList.toggle('active');
+    document.getElementById('activeBSA').classList.toggle('bachelorOpen');
+
+    if(displaySetting == 'block'){
+        BSACourse.style.display = 'none'
+    }
+    else{
+        BSACourse.style.display = 'block'
+    }
+}
+
+function toggleArrowBSAD(){
+    var arrowToggle = document.querySelector('#toggleBSAD');
+    var BSADCourse = document.getElementById('courseDetailsBSAD');
+    var displaySetting = BSADCourse.style.display;
+    arrowToggle.classList.toggle('active');
+    document.getElementById('activeBSAD').classList.toggle('bachelorOpen2');
+
+    if(displaySetting == 'block'){
+        BSADCourse.style.display = 'none'
+    }
+    else{
+        BSADCourse.style.display = 'block'
+    }
+}
+
+function toggleArrowBSADMMM(){
+    var arrowToggle = document.querySelector('#toggleBSADMMM');
+    var BSADMMMCourse = document.getElementById('courseDetailsBSADMMM');
+    var displaySetting = BSADMMMCourse.style.display;
+    arrowToggle.classList.toggle('active');
+    document.getElementById('activeBSADMMM').classList.toggle('bachelorOpen2');
+
+    if(displaySetting == 'block'){
+        BSADMMMCourse.style.display = 'none'
+    }
+    else{
+        BSADMMMCourse.style.display = 'block'
+    }
+}
+
+function toggleArrowBSADHR(){
+    var arrowToggle = document.querySelector('#toggleBSADHR');
+    var BSADHRCourse = document.getElementById('courseDetailsBSADHR');
+    var displaySetting = BSADHRCourse.style.display;
+    arrowToggle.classList.toggle('active');
+    document.getElementById('activeBSADHR').classList.toggle('bachelorOpen2');
+
+    if(displaySetting == 'block'){
+        BSADHRCourse.style.display = 'none'
+    }
+    else{
+        BSADHRCourse.style.display = 'block'
+    }
+}
+
+function toggleArrowBSPUBAD(){
+    var arrowToggle = document.querySelector('#toggleBSPUBAD');
+    var BSPUBADCourse = document.getElementById('courseDetailsBSPUBAD');
+    var displaySetting = BSPUBADCourse.style.display;
+    arrowToggle.classList.toggle('active');
+    document.getElementById('activeBSPUBAD').classList.toggle('bachelorOpen');
+
+    if(displaySetting == 'block'){
+        BSPUBADCourse.style.display = 'none'
+    }
+    else{
+        BSPUBADCourse.style.display = 'block'
+    }
+}
+
+
+
+$('input').on('change', function() {
+    $('body').toggleClass('blue');
+  });
+$('input').on('change', function() {
+  $('body').toggleClass('blue');
+});
+  
